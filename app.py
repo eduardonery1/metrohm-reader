f20c58549abe006f54e3dbe6a3faf3eaee905e98
Eduardo Nery <eduardo.nery8@gmail.com>
2025-04-04 00:53:45 +0000

    Em português mesmo

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 
-st.title("Metrohm data extractor")
+st.title("Extrator de métricas")
 uploaded_file = st.file_uploader("Selecione arquivo XLSX exportado pelo Metrohm", type=["xlsx"])
 
 if uploaded_file:
@@ -19,17 +19,17 @@ if uploaded_file:
         st.error(f"Error reading the Excel file: {e}")
         st.stop()
 
-    st.write("### Data Preview", df.head())
-    st.write(f"**Total Rows:** {len(df)}")
-    st.write(f"**Total Columns:** {len(df.columns)}")
+    st.write("### Preview", df.head())
+    st.write(f"**Número de linhas:** {len(df)}")
+    st.write(f"**Número de colunas:** {len(df.columns)}")
     
     for i in range(1, df['Scan'].max() + 1):
         df_scan = df.where(df['Scan'] == i).dropna(how='all')
-        st.write(f"### Scan {i} Data")
+        st.write(f"### Scan {i}")
         st.write(df_scan.head())
 
-        st.write(f"**Scan {i} - Number of points:** {len(df_scan)}")
-        st.write(f"**Scan {i} - Description of Current.**")
+        st.write(f"**Número de pontos:** {len(df_scan)}")
+        st.write(f"**Descrição da corrente gerada:**")
         st.write(df_scan['WE(1).Current (A)'].describe())
 
         # Plot
@@ -43,8 +43,8 @@ if uploaded_file:
         min_current_reverse = df['WE(1).Current (A)'].min()
         min_potential_reverse = df.loc[df['WE(1).Current (A)'].idxmin(), 'Potential applied (V)']
 
-        st.write(f"**Scan {i} - Current Mean:** {current_mean:.2e}")
-        st.write(f"**Scan {i} - Current Standard Deviation:** {current_std:.2e}")
-        st.write(f"**Scan {i} - Current Peak (Forward):** {peak_current_forward:.2e} at {peak_potential_forward:.2f} V")
-        st.write(f"**Scan {i} - Current Minimum (Reverse):** {min_current_reverse:.2e} at {min_potential_reverse:.2f} V")
+        st.write(f"**Mediana da corrente:** {current_mean:.2e}")
+        st.write(f"**Desvio padrão da corrente:** {current_std:.2e}")
+        st.write(f"**Pico da corrente (Forward):** {peak_current_forward:.2e} at {peak_potential_forward:.2f} V")
+        st.write(f"**Vale da corrente (Reverse):** {min_current_reverse:.2e} at {min_potential_reverse:.2f} V")
 
